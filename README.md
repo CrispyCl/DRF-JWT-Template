a18d7036dc5a4f70f79e2ff1a4a5c47518a7d8c8
Gogasmerter <chebunin19@gmail.com>
2024-04-06 18:01:31 +0400

    Add DjangoAPP with flake and black configs

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# DRF-JWT-Template
+# DjangoApp API
 Django Rest Framework API with JWT authentication Template
+
+# Starting the project in dev-mode
+
+## Set up and activate the virtual environment
+
+ ```bash
+python -m venv venv
+source venv/bin/activate
+```
+
+In Windows, the command will be different:
+```bat
+venv\Scripts\activate
+```
+
+## Establish dependencies
+
+* For development:
+
+    ```bash
+    pip install -r requirements/dev.txt
+    ```
+
+* For production:
+
+    ```bash
+    pip install -r requirements/prod.txt
+    ```
+
+* For testing:
+
+    ```bash
+    pip install -r requirements/test.txt
+    ```
+
+## Create an ".env" file in the root folder
+
+```bash
+cp .env.template .env
+```
+
+In Windows, the command will be different:
+
+```bat
+copy .env.template .env
+```
+
+If the ".env" file is not created, the default values will be used
+
+### Settings .env
+
+1. DJANGO_SECRET_KEY - secret key, string (DJANGO_SECRET_KEY="secret_key")
+2. DJANGO_DEBUG - development mode, False if empty string (DJANGO_DEBUG="")
+3. DJANGO_ALLOWED_HOSTS - allowed hosts: list of IP-addresses divided by " " (DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1")
+4. DJANGO_INTERNAL_IPS - allowed hosts on the local machien, list of IP-addresses divided by " " (DJANGO_INTERNAL_IPS="localhost 127.0.0.1")
+
+## Go to the project folder
+
+```bash
+cd app
+```
+
+## Database Creation
+
+### Apply migrations
+
+```bash
+python manage.py migrate
+```
+
+## Admin panel
+
+### Create an admin user
+
+```bash
+python manage.py createsuperuser
+```
+Follow the instructions
+
+## Server startup
+
+```bash
+python manage.py runserver
+```
---
+++ app/app/settings.py
@@ -0,0 +1,104 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(override=False)
+
+# Common settings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "secret_key")
+DEBUG = os.getenv("DJANGO_DEBUG", "true").lower() in ("true", "1", "yes", "y")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(" ")
+INTERNAL_IPS = os.getenv("DJANGO_INTERNAL_IPS", "127.0.0.1").split(" ")
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    # Django apps
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+]
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "app.wsgi.application"
+ROOT_URLCONF = "app.urls"
+
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Auth settings
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "static/"
