b5a8d2a89153d330c26605f360cbb3969a3f6ce1
Gogasmerter <chebunin19@gmail.com>
2024-04-06 18:55:18 +0400

    Add JWT Aunthentication

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt import views as rest_views
 
 urlpatterns = [
+    path("api/v1/token/", rest_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token/refresh/", rest_views.TokenRefreshView.as_view(), name="token_refresh"),
     path("admin/", admin.site.urls),
 ]
 
